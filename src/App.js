7452640a55ab13021642e043ac29c45b18df4d17
ajtheengineer <ajtheengineer@gmail.com>
2022-06-21 21:28:07 -0200

    Update copy to Start Playing Wordle Clone!

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Start playing Wordle!
+          Start playing Wordle Clone!
         </a>
       </header>
     </div>
