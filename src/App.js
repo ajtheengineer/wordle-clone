8c47c9e47931bbf5d2d01b9e89d2442cec1f1d78
ajtheengineer <ajtheengineer@gmail.com>
2022-06-21 21:41:41 -0200

    Add header component that says Wordle-Clone
    Copied some styling from NYT website, couldn't get their
    custom font, so went with Roboto.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import Header from './components/header.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Let's play Wordle Clone. </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Start playing Wordle Clone!
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
     </div>
   );
 }
