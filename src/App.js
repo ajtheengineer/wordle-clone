4830403a4f28f4cbe2a61eb53a59302a9b7a2fba
ajtheengineer <ajtheengineer@gmail.com>
2022-06-21 23:10:16 -0200

    Add grid component
    This contains all the tiles on our game.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './app.css';
+import Grid from './components/grid.js';
 import Header from './components/header.js';
 
 function App() {
+  const width = 5
+  const height = 6
+
   return (
     <div className="app-container">
       <Header />
+      <Grid width={width} height={height} />
     </div>
   );
 }
